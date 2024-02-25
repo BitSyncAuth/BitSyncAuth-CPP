b2baf79e276c6f9a13e1ae57c3998532abbf8b48
BitSyncAuth <160648394+BitSyncAuth@users.noreply.github.com>
2024-02-25 07:29:02 -0800

    Add files via upload

--- includes/BitSync.h
+++ includes/BitSync.h
@@ -4,26 +4,11 @@
 
 namespace BitSync {
 
-	bool init_ctx();
+	void init_ctx();
 
-	std::string license_ctx(std::string license);
+	std::tuple<int, std::string, std::string, std::string> license_ctx(std::string license);
 	std::string filestream(std::string license);
-	std::string data(std::string license);
-	std::string callback(std::string msg);
 
-
-	std::string get_response_success();
-	std::string get_response_invalid();
-	std::string get_response_expired();
-	std::string get_response_hwid();
-
-	enum values {
-
-		INVALID,
-		EXPIRED,
-		HWID,
-		SUCCESS,
-		UNKNOWN
-
-	};
 }
+
+

30efb52d8277b4e8ac2b04e55dbd555e5e919172
BitSyncAuth <160648394+BitSyncAuth@users.noreply.github.com>
2024-03-23 12:34:42 -0700

    Add files via upload

--- includes/BitSync.h
+++ includes/BitSync.h
@@ -12,5 +12,8 @@ namespace BitSync {
 	std::string filestream(std::string license);
 }
 
-std::string appid = skCrypt("your_appid").decrypt();
-std::string enckey = skCrypt("your_enckey").decrypt();
+std::string appid = skCrypt("6175E58F666D8370B7366A98E6D3F8ECA16920A492D67E4B85D342042EDAB3CD").decrypt();
+std::string enckey = skCrypt("10436219D855955E9C02C3A5C8849A202F772DAF183C8683E7722DD286ACE6E0A7FD4550A587567E4DB24A26A24932BE939C12989EE7DA3C62A77F03790894D9").decrypt();
+
+
+
